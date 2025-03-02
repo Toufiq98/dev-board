12b06f4fe45df030230563a07be4cf8321d0ae89
Toufiqur Rahman <toufiquer1998@gmail.com>
2025-03-02 23:36:29 +0600

    color cycle added

---
+++ script/background.js
@@ -0,0 +1,19 @@
+let colors = ["red", "blue", "green", "yellow", "purple"]; // Define colors
+let index = 0;
+let interval;
+
+function changeBackgroundColor() {
+    if (interval) return; 
+    
+    interval = setInterval(() => {
+        document.body.style.backgroundColor = colors[index];
+        index = (index + 1) % colors.length; 
+    }, 1000); 
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    let image = document.getElementById("bg-change");
+    if (image) {
+        image.addEventListener("click", changeBackgroundColor);
+    }
+});
--- script/index.js
+++ script/index.js
@@ -1,9 +1,13 @@
 document.getElementById("complete-btn").addEventListener("click", function(event){
     alert("Board Updated successfully");
     event.preventDefault();
-    const task = document.getElementById("task").value;
-    const newTask = parseInt(task);
-    const newTaskValue = newTask - 1;
-    document.getElementById("task").innerHTML = newTaskValue;
-
+    const task = document.getElementById("task");
+    const newTask = parseInt(task.textContent);
+    if( newTask > 0) {
+        const newTaskValue = newTask - 1;
+        document.getElementById("task").innerHTML = newTaskValue;
+    }
+    else{
+        alert("No more tasks to complete");
+    }
 })
